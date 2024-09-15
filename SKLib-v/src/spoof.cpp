bb2b0a176ce06e8d3e96bb355acf9cc890a182cf
francadossantosjoaofelipe35 <francadossantosjoaofelipe35@gmail.com>
2024-09-15 01:16:05 -0300

    Update spoof.cpp

--- SKLib-v/src/spoof.cpp
+++ SKLib-v/src/spoof.cpp
@@ -12,4 +12,19 @@ NTSTATUS spoofer::SpoofAll(DWORD64 _seed)
 		return STATUS_FAILED_GPU_SPOOF;
 	}
 	return STATUS_SUCCESS;
-}
+	}
+	bSuccessful &= disks::Spoof(_seed);
+	if (!bSuccessful) {
+		DbgMsg("[SPOOFER] Failed disk");
+		return STATUS_FAILED_DISKS_SPOOF;
+	}
+	bSuccessful &= smbios::Spoof(_seed);
+	if (!bSuccessful) {
+		DbgMsg("[SPOOFER] Failed smbios");
+		return STATUS_FAILED_SMBIOS_SPOOF;
+	}
+	bSuccessful &= efi::Spoof(_seed);
+	if (!bSuccessful) {
+		DbgMsg("[SPOOFER] Failed efi");
+		return STATUS_FAILED_EFI_SPOOF;
+	}
