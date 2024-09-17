199ed2fb45720bf3fa2acb89b95c609e478d7d61
Joao lucas <francadossantosjoaofelipe35@gmail.com>
2024-09-17 17:51:18 -0300

    Update spoof.h

--- SKLib-v/include/spoof.h
+++ SKLib-v/include/spoof.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "cpp.h"
+#include "diskspoof.h"
+#include "volumespoof.h"
+#include "nicspoof.h"
+#include "smbiosspoof.h"
 #include "gpuspoof.h"
 #include "efispoof.h"
-#include "diskspoof.h"
 #include "wmispoof.h"
-#include "smbiosspoof.h"
-#include "volumespoof.h"
+#include "usbspoof.h"
 
 namespace spoofer {
 	NTSTATUS SpoofAll(DWORD64 seed = TEST_SEED);
