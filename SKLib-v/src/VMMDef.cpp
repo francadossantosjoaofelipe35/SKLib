6d407a029d2d44211ed1cd60425efc11ec75141d
Joao lucas <francadossantosjoaofelipe35@gmail.com>
2024-09-17 18:02:39 -0300

    Update VMMDef.cpp

--- SKLib-v/src/VMMDef.cpp
+++ SKLib-v/src/VMMDef.cpp
@@ -76,9 +76,6 @@ void vmm::Init()
 
     DbgMsg("[VMM] Initialising custom bugcheck");
     bugcheck::Init();
-	
-	DbgMsg("[VMM] Initialising EAC bypasses");
-	eac::Init();
 
     PEPROCESS ntoskrnl = PsInitialSystemProcess;
 	vmm::guestCR3.Flags = *(DWORD64*)((DWORD64)ntoskrnl + 0x28);
