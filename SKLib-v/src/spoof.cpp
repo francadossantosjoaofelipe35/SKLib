fd2d5c939bea158e60694cf546c6168aef3d43f2
Joao lucas <francadossantosjoaofelipe35@gmail.com>
2024-09-17 20:47:00 -0300

    Update spoof.cpp

--- SKLib-v/src/spoof.cpp
+++ SKLib-v/src/spoof.cpp
@@ -16,11 +16,6 @@ NTSTATUS spoofer::SpoofAll(DWORD64 _seed)
 		DbgMsg("[SPOOFER] Failed disk");
 		return STATUS_FAILED_DISKS_SPOOF;
 	}
-	bSuccessful &= smbios::Spoof(_seed);
-	if (!bSuccessful) {
-		DbgMsg("[SPOOFER] Failed smbios");
-		return STATUS_FAILED_SMBIOS_SPOOF;
-	}
 	bSuccessful &= efi::Spoof(_seed);
 	if (!bSuccessful) {
 		DbgMsg("[SPOOFER] Failed efi");
