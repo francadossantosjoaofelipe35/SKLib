6ffc47e719d1cadba16d784cff043e269d65b0ea
Joao lucas <francadossantosjoaofelipe35@gmail.com>
2024-09-18 16:39:03 -0300

    Update gpuspoof.h

--- SKLib-v/include/gpuspoof.h
+++ SKLib-v/include/gpuspoof.h
@@ -1,28 +1,8 @@
 #pragma once
 
-#include <Windows.h>
-#include <Zydis/Zydis.h>
-#include "spoof.h"
-#include "Memory.h"
-#include "DbgMsg.h"
-#include <intrin.h>  // Para _disable() e _enable()
+#include "cpp.h"
 
-// Definições de constantes para os limites de GPUs
-#define NV_MAX_DEVICES 32
-
-// Variáveis globais para offsets e base do driver
-extern DWORD64 pGpuSystem;
-extern DWORD32 gpuSysOffset;
-extern DWORD32 gpuMgrOffset;
-extern DWORD32 gpuSysOffset2;
-extern DWORD32 bInitOffset;
-extern DWORD32 uuidOffset;
-
-// Funções de spoof para GPU
-namespace gpu {
-    bool Spoof(DWORD64 seed);  // Função que realiza o spoof das GPUs
+namespace gpuspoof {
+    // Função que realiza o spoof do UUID da GPU utilizando a seed para gerar valores aleatórios
+    NTSTATUS SpoofGPU(DWORD64 seed);
 }
-
-// Funções auxiliares para trabalhar com as GPUs
-DWORD64 gpuData(DWORD32 gpuInstance);  // Retorna os dados da GPU
-DWORD64 nextGpu(DWORD32 deviceMask, DWORD32* startIndex);  // Itera sobre as GPUs
