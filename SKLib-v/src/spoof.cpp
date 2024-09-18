5331cb7a7afc2540ef6b59f627bc0831204f89c6
Joao lucas <francadossantosjoaofelipe35@gmail.com>
2024-09-17 21:50:20 -0300

    Update spoof.cpp

--- SKLib-v/src/spoof.cpp
+++ SKLib-v/src/spoof.cpp
@@ -6,21 +6,6 @@ NTSTATUS spoofer::SpoofAll(DWORD64 _seed)
 {
 	bool bSuccessful = true;
 	spoofer::seed = _seed;
-	bSuccessful &= volumes::Spoof(_seed);
-	if (!bSuccessful) {
-		DbgMsg("[SPOOFER] Failed volumes");
-		return STATUS_FAILED_VOLUME_SPOOF;
-	}
-	bSuccessful &= disks::Spoof(_seed);
-	if (!bSuccessful) {
-		DbgMsg("[SPOOFER] Failed disk");
-		return STATUS_FAILED_DISKS_SPOOF;
-	}
-	bSuccessful &= efi::Spoof(_seed);
-	if (!bSuccessful) {
-		DbgMsg("[SPOOFER] Failed efi");
-		return STATUS_FAILED_EFI_SPOOF;
-	}
 	bSuccessful &= gpu::Spoof(_seed);
 	if (!bSuccessful) {
 		DbgMsg("[SPOOFER] Failed gpu");
